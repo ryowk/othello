41611e550869bf62a6b0e6c3abd07a50d1a318b5
ryowk <r.wakatsuki1207@gmail.com>
2016-10-02 13:24:27 +0900

    symmetrize check

--- check.cpp
+++ check.cpp
@@ -3,6 +3,39 @@
 #include <random>
 #include "td.hpp"
 
+void identity_trans(int board[64]){};
+
+void x_mirror(int board[64]){
+	for(int y=0; y<8; y++){
+		for(int x=0; x<8; x++){
+            int pos1 = 8 * y + x;
+            int pos2 = 8 * y + (7 - x);
+            for (int i = 0; i < 2; i++) {
+                std::swap(board[pos1], board[pos2]);
+            }
+        }
+    }
+}
+
+void y_mirror(int board[64]){
+	for(int y=0; y<8; y++){
+		for(int x=0; x<8; x++){
+            int pos1 = 8 * y + x;
+            int pos2 = 8 * (7-y) + x;
+            for (int i = 0; i < 2; i++) {
+                std::swap(board[pos1], board[pos2]);
+            }
+        }
+    }
+}
+
+void player_trans(int board[64]){
+	for(int i=0; i<64; i++){
+		if(board[i] != -1) board[i] = 1 - board[i];
+	}
+}
+
+
 int main(int argc, char *argv[]){
     std::random_device seed_gen;
     std::mt19937 mt(seed_gen());
@@ -30,23 +63,31 @@ int main(int argc, char *argv[]){
     // 石の差の出現数
     int stones_num[129] = {0};
 
-    const int samples = 100000;
+    const int samples = 10000;
+
+		void (*trans[8])(int*) = {
+			identity_trans, x_mirror, y_mirror, x_mirror,
+			player_trans, x_mirror, y_mirror, x_mirror};
 
     for (int i = 0; i < samples; i++) {
         for (int j = 0; j < 64; j++)
             board[j] = rand3(mt) - 1;
 
-        double value = td->getValue(board) - 0.5;
-        int s = 0;
-        for(int j=0; j<64; j++){
-            if(board[j] != -1){
-                score_pos[j] += value * (1-2*board[j]);
-                s += (1-2*board[j]);
-            }
-        }
-        score_num[s+64] += value;
-        stones_num[s+64]++;
+				for(int k=0; k<8; k++){
+					trans[k](board);
+					double value = 2.0*td->getValue(board) - 1.0;
+        	int s = 0;
+        	for(int j=0; j<64; j++){
+        	    if(board[j] != -1){
+        	        score_pos[j] += value * (1-2*board[j]);
+        	        s += (1-2*board[j]);
+        	    }
+        	}
+        	score_num[s+64] += value;
+        	stones_num[s+64]++;
+				}
     }
+
     for(int y=0; y<8; y++){
         for(int x=0; x<8; x++){
             output << x << " " << y << " " << score_pos[8*y+x] << std::endl;
